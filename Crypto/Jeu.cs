3b4e75f815fcad9c240e2adbf5348a9d3c569fd1
Red71300 <moon_shadow_s@hotmail.fr>
2021-02-17 23:29:00 +0100

    Etape 3

--- Crypto/Jeu.cs
+++ Crypto/Jeu.cs
@@ -55,9 +55,9 @@ namespace Crypto
         //mélange complet afin de déterminer la clé complète à l'aide du paquet de cartes
         public void Melanger()
         {
-            //recul du joker noir
-            //Move("Joker-noir", -1);
-            
+            Move("Joker-noir", -1); //recul du joker noir
+            Move("Joker-rouge", -2); //recul du joker rouge
+            DoubleCoupe();
         }
 
         //déplace une carte dans le paquet
@@ -65,8 +65,17 @@ namespace Crypto
         {
             int position = FindPosition(nom);
             Carte carte = this.cartes[position];
-            this.cartes[position] = this.cartes[position - nb];
-            this.cartes[position - nb] = carte;
+
+            if ((position - nb) <= 53)
+            {
+                this.cartes[position] = this.cartes[position - nb];
+                this.cartes[position - nb] = carte;
+            }
+            else
+            {
+                this.cartes[position] = this.cartes[-nb];
+                this.cartes[-nb] = carte;
+            }
         }
 
         //récupère la position d'une carte dans le paquet
@@ -83,6 +92,80 @@ namespace Crypto
                 i++;
             }
             return position;
+        }
+
+        //double coupe des deux jokers
+        void DoubleCoupe()
+        {
+            int posJokerR = FindPosition("Joker-rouge");
+            int posJokerN = FindPosition("Joker-noir");
+            List<Carte> listeDebut = new List<Carte>();
+            List<Carte> listeFin = new List<Carte>();
+            List<Carte> listeMilieu = new List<Carte>();
+            List<Carte> listeFinale = new List<Carte>();
+
+            //si le joker rouge est avant le joker noir
+            if (posJokerN > posJokerR)
+            {
+                //init liste du début du paquet
+                for (int i = 0; i < posJokerR; i++)
+                {
+                    listeDebut.Add(this.cartes[i]);
+                }
+
+                //init liste du milieu en incluant les deux jokers
+                for (int i = posJokerR; i <= posJokerN; i++)
+                {
+                    listeMilieu.Add(this.cartes[i]);
+                }
+
+                //init liste de fin du paquet
+                for (int i = posJokerN + 1; i < 54; i++)
+                {
+                    listeFin.Add(this.cartes[i]);
+                }
+            }
+            //si le joker noir est avant le joker rouge
+            else
+            {
+                //init liste du début du paquet
+                for (int i = 0; i < posJokerN; i++)
+                {
+                    listeDebut.Add(this.cartes[i]);
+                }
+
+                //init liste du milieu en incluant les deux jokers
+                for (int i = posJokerN; i <= posJokerR; i++)
+                {
+                    listeMilieu.Add(this.cartes[i]);
+                }
+
+                //init liste de fin du paquet
+                for (int i = posJokerR + 1; i < 54; i++)
+                {
+                    listeFin.Add(this.cartes[i]);
+                }
+            }
+
+            //init la liste finale
+            foreach(Carte carte in listeFin)
+            {
+                listeFinale.Add(carte);
+            }
+            foreach (Carte carte in listeMilieu)
+            {
+                listeFinale.Add(carte);
+            }
+            foreach (Carte carte in listeDebut)
+            {
+                listeFinale.Add(carte);
+            }
+
+            //set nouvel ordre
+            for (int i=0; i<54; i++)
+            {
+                this.cartes[i] = listeFinale[i];
+            }
         }
     }
 }
